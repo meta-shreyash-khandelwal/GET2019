399b60c5da0f66e06a7ca51e205da06598a4bf08
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-19 14:27:08 +0530

    Update NqueenTest.java

--- Assignment-5 SCF(Recursion) July 19/NqueenTest.java
+++ Assignment-5 SCF(Recursion) July 19/NqueenTest.java
@@ -4,11 +4,29 @@ import org.junit.Test;
 public class NqueenTest {
 
 	@Test
-	public void test() {
-		int board[][]=new int[4][4];
-		assertEquals(true,NQueen.nQueen(board,0,4));
-		
-		fail("Not yet implemented");
+	public void testNqueen1() {
+		int board[][] = new int[4][4];
+		assertTrue(NQueen.nQueenCheck(board, 0, 4));
 	}
 
+	@Test
+	public void testNqueen2(){
+		int board[][] = new int[8][8];
+		assertTrue(NQueen.nQueenCheck(board, 0, 8));
+	}
+
+	@Test
+	public void testNqueen3(){
+			int board[][] = new int[3][3];
+		System.out.println("Return Value is" + NQueen.nQueenCheck(board, 0, 3));
+		assertFalse(NQueen.nQueenCheck(board, 0, 3));
+	}
+
+	@Test
+	public void testNqueen4(){
+			int board[][] = new int[2][2];
+		System.out.println("Return value is " + NQueen.nQueenCheck(board, 0, 2));
+		assertFalse(NQueen.nQueenCheck(board, 0, 2));
+	}
+	
 }
