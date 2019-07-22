712fd8944dec254bdf7de99769a40b7f901ad8b0
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-22 07:58:35 +0530

    Update Poly.java

--- Assignment-6 SCF(ADT & CDT)July 22/Poly.java
+++ Assignment-6 SCF(ADT & CDT)July 22/Poly.java
@@ -1,7 +1,48 @@
 package varunk;
-
+import java.util.*;
 public class Poly {
+Map<Integer,Integer> polynomial;
+public Poly()
+{
+	polynomial=new HashMap<Integer,Integer>();
+}
+double evaluate(float coefficient)
+{
+double result=0;
+for(Integer key:polynomial.keySet())
+{
+	result+=Math.pow(coefficient, key)*polynomial.get(key);
+}
+return result;
+}
 
+int degree()
+{
+	int maximumDegree=-1;
+	for(Integer key:polynomial.keySet())
+	{
+		if(key>maximumDegree)
+			maximumDegree=key;
+	}
+	return maximumDegree;
+}
+
+Map<Integer,Integer> addPoly(Poly firstPolynomial, Poly secondPolynomial)
+{
+	int firstDegree=firstPolynomial.degree();
+	int secondDegree=secondPolynomial.degree();
+polynomial
+		
+}
+
+Map<Integer,Integer> multiplyPoly(Poly firstPolynomial, Poly secondPolynomial) 
+{
+	
+}
+
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
