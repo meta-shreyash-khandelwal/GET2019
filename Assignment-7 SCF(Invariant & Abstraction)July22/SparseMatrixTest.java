eea39e62043a948776b28f31ca106a03a1f4b7fe
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-22 16:41:48 +0530

    Add files via upload

---
+++ Assignment-7 SCF(Invariant & Abstraction)July22/SparseMatrixTest.java
@@ -0,0 +1,62 @@
+package varunk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SparseMatrixTest {
+
+	@Test
+	public void transposeArrayTest1() {
+	 int sparseMatrix[][]=new int[][]{{0,3,1},{1,1,5},{2,2,3},{3,0,9},{3,4,10}};
+	 int transposeMatrix[][]=new int[][]{{0,3,9},{1,1,5},{2,2,3},{3,0,1},{4,3,10}};
+		assertArrayEquals(transposeMatrix,SparseMatrix.transposeArrayOpertion(sparseMatrix));
+		
+	}
+	@Test
+	public void symmetricArrayTest1() {
+	 int sparseMatrix[][] = new int[][] { { 0, 0, 4 }, { 0, 1, 1 }, { 0, 2, 2 },
+				{ 0, 3, 5 }, { 0, 4, 2 }, { 1, 0, 1 }, { 1, 1, 5 },
+				{ 2, 0, 2 }, { 2, 2, 3 }, { 3, 0, 5 }, { 3, 3, 6 },
+				{ 4, 0, 2 }, { 4, 4, 16 } };
+		assertTrue(SparseMatrix.symmetricArrayOperation(sparseMatrix));
+		
+		
+	}
+	@Test
+	public void symmetricArrayTest2() {
+	 int sparseMatrix[][] = new int[][] { { 1, 0, 4 }, { 0, 1, 1 }, { 0, 2, 2 },
+				{ 0, 3, 5 }, { 0, 4, 2 }, { 1, 0, 1 }, { 1, 1, 5 },
+				{ 2, 0, 2 }, { 2, 2, 3 }, { 3, 0, 5 }, { 3, 3, 6 },
+				{ 4, 0, 2 }, { 4, 4, 16 } };
+		assertFalse(SparseMatrix.symmetricArrayOperation(sparseMatrix));
+		
+		
+	}
+	@Test
+	public void additionMatrixTest1()
+	{
+		int sparseMatrix1[][] = new int[][] { { 1, 2, 3 }, { 2, 1, 4 }, { 3, 4, 3 } };
+		int sparseMatrix2[][] = new int[][] { { 0, 3, 4 }, { 1, 2, 5 }, { 1, 3, 1 },
+				{ 2, 4, 3 }, { 3, 1, 1 }};
+		int sparseMatrix3[][]=new int[][]{{0,3,4},{0,0,0},{1,2,8},{1,3,1},{2,1,4},{2,4,3},{3,4,3},{3,1,1}};
+		assertArrayEquals(sparseMatrix3,SparseMatrix.addMatrices(sparseMatrix1, sparseMatrix2));
+		
+	}
+	@Test
+	public void multiplyMatrixTest1()
+	{
+		int sparseMatrix1[][] = new int[][] { { 0, 0, 0, 1, 0 }, { 3, 0, 0, 0, 0 },
+				{ 0, 0, 2, 0, 0 }, { 0, 0, 0, 0, 9 } };
+
+		int sparseMatrix2[][] = new int[][] { { 0, 6, 0, 0 }, { 0, 0, 0, 7 },
+				{ 1, 0, 0, 0 }, { 0, 0, 2, 0 }, { 0, 8, 0, 0 } };
+		int sparseMatrix3[][]=new int[][]{{0,2,2},{1,1,18},{2,0,2},{3,1,72}};
+		assertArrayEquals(sparseMatrix3,SparseMatrix.multiplyMatrices(sparseMatrix1, sparseMatrix2));
+		
+	}
+	
+	
+	
+
+}
