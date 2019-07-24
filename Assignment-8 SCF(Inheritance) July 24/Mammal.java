14b34b92c0c3af54a2fc7db1705a3940b30f39bf
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-24 15:46:17 +0530

    Update Mammal.java

--- Assignment-8 SCF(Inheritance) July 24/Mammal.java
+++ Assignment-8 SCF(Inheritance) July 24/Mammal.java
@@ -1,11 +1,13 @@
-package varunk;
-
 public class Mammal extends Animal {
 	public Mammal(String uniqueName, int age, double weight, String category2,
-			String type) {
+			      String type) {
 		super(uniqueName, age, weight, category2, type);
 	}
-
+/**
+ * This method is getter for category like mammal,reptiles or bird
+ * @return the category
+ */
+	//Override the superclass's functions
 	public String getCategory() {
 		return super.category;
 	}
