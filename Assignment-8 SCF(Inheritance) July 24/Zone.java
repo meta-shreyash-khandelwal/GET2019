2a6908e5271c3ac6bba03bd525bf77e916d5f877
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-24 16:17:30 +0530

    Update Zone.java

--- Assignment-8 SCF(Inheritance) July 24/Zone.java
+++ Assignment-8 SCF(Inheritance) July 24/Zone.java
@@ -1,7 +1,3 @@
-package varunk;
-
-import java.util.*;
-
 public class Zone {
 	String category;
 	List<Cage> objectCage;
@@ -10,14 +6,19 @@ public class Zone {
 	boolean hasCanteen;
 
 	public Zone(int limitOfCages, boolean hasPark, boolean hasCanteen,
-			String category) {
+			    String category) {
 		this.hasPark = hasPark;
 		this.hasCanteen = hasCanteen;
 		this.limitOfCages = limitOfCages;
 		this.category = category;
 
 	}
-
+/**
+ * This method add the cages to the zones according to the specified call
+ * @param type is the animal type like lion,peacock or crocodile
+ * @param capacity is the number of animals of same type in a single cage
+ * @return the list of objects of Cages in a specified zone
+ */
 	public List<Cage> addCages(String type, int capacity) {
 		objectCage = new ArrayList<Cage>(capacity);
 		for (int i = 0; i < limitOfCages; i++) {
@@ -25,7 +26,10 @@ public class Zone {
 		}
 		return objectCage;
 	}
-
+/**
+ * This method adds the animal to the cage in a particular zone
+ * @param cageObject is the list containing the objects of cages
+ */
 	public void addAnimalToCage(List<Cage> cageObject) {
 		int counter = 0;
 		for (int i = 0; i < limitOfCages; i++) {
@@ -36,16 +40,19 @@ public class Zone {
 
 			}
 		}
-		try{
-		if (counter == 0) {
-			throw new Exception("Full Capacity");
-			//System.out.println("FULL Capacity");
-		}}catch(Exception e)
-		{
-			System.out.println("FULL Capacity Not able to add");
+		try {
+			if (counter == 0) {
+				throw new Exception("Full Capacity");
+				// System.out.println("FULL Capacity");
+			}
+		} catch (Exception e) {
+			System.out.println("FULL Capacity Not able to add animal !!");
 		}
 	}
-
+/**
+ * This method deletes the animal from the cage in a specified cage in a zone
+ * @param cageObject is the list containing the cages after deleting the animal from cage 
+ */
 	public void deathOfAnimal(List<Cage> cageObject) {
 		int counter = 0;
 		if (limitOfCages == 0) {
@@ -59,12 +66,11 @@ public class Zone {
 
 				}
 			}
-			try{
-			if (counter == 0) {
-				throw new Exception("No Animal");
-			}}
-			catch(Exception e)
-			{
+			try {
+				if (counter == 0) {
+					throw new Exception("No Animal");
+				}
+			} catch (Exception e) {
 				System.out.println("No animals in Cages to delete");
 			}
 
