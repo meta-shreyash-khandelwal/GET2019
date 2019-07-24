329b3fc2f7bf28c700930d6d55bb03eba5526f4c
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-24 14:30:10 +0530

    Update Animal.java

--- Assignment-8 SCF(Inheritance) July 24/Animal.java
+++ Assignment-8 SCF(Inheritance) July 24/Animal.java
@@ -6,8 +6,6 @@ public class Animal {
 	String category;
 	String type;
 	String sound;
-
-	// String id;
 	public Animal(String uniqueName, int age, double weight, String category,
 			String type) {
 		this.uniqueName = uniqueName;
@@ -16,27 +14,45 @@ public class Animal {
 		this.category = category;
 		this.type = type;
 	}
-
+/**
+ * This is getter method for sound
+ * @return sound of animal
+ */
 	public String getSound() {
 		return sound;
 	}
-
+/**
+ * This is getter method for weight
+ * @return weight of animal
+ */
 	public double getWeight() {
 		return weight;
 	}
-
+/**
+ * This is getter method for age
+ * @return age of animal
+ */
 	public int getAge() {
 		return age;
 	}
-
+/**
+ * This method is getter for category like mammal,reptiles or bird
+ * @return the category
+ */
 	public String getCategory() {
 		return category;
 	}
-
+/**
+ * This method is getter for type such as lion,crocodile or peacock
+ * @return
+ */
 	public String getType() {
 		return type;
 	}
-
+/**
+ * This method is getter for name of Animal
+ * @return the unique name of Animal
+ */
 	public String getName() {
 		return uniqueName;
 	}
