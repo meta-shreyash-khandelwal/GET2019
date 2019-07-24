784507afc45bf793acf72d0cdbb4b97549aee0bb
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-24 15:47:23 +0530

    Update Peacock.java

--- Assignment-8 SCF(Inheritance) July 24/Peacock.java
+++ Assignment-8 SCF(Inheritance) July 24/Peacock.java
@@ -1,16 +1,14 @@
-package varunk;
-
 public class Peacock extends Bird {
 	String sound;
 	public Peacock(String uniqueName, int age, double weight, String category,
-			String type) {
+		       String type) {
 		super(uniqueName, age, weight, category, type);
 	}
-
+//Over-riding to show sound of specific animal's sound
 	public String getSound() {
 		return "chirp";
 	}
-
+/* Over-riding methods  */
 	public double getWeight() {
 		return weight;
 	}
