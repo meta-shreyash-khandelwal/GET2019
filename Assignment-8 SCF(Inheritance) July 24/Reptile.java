5e334994a8bb54e3e54713ef987f7d61428bba58
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-24 15:49:08 +0530

    Update Reptile.java

--- Assignment-8 SCF(Inheritance) July 24/Reptile.java
+++ Assignment-8 SCF(Inheritance) July 24/Reptile.java
@@ -1,9 +1,7 @@
-package varunk;
-
 public class Reptile extends Animal {
 	public Reptile(String uniqueName, int age, double weight, String category2,
-			String type) {
-		super(uniqueName, age, weight, category2, type);
+		       String type) {
+		super(uniqueName, age, weight, category2, type);//Calling super class's constructor
 	}
 
 }
