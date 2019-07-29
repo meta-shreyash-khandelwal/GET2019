c59ed7f903b1662406e9ca72b05cc12704b97118
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-29 11:51:54 +0530

    Update Triangle.java

--- Assignment-9 SCF(Factory Method) July 25/Triangle.java
+++ Assignment-9 SCF(Factory Method) July 25/Triangle.java
@@ -1,80 +1,75 @@
-package varunk;
+
 import java.util.*;
 public class Triangle implements Shape {
-private double sideFirst;
-private double sideSecond;
-private double sideThird;
+	private double sideFirst;
+	private double sideSecond;
+	private double sideThird;
+	private Point shapeOrigin;
 
-Point   shapeOrigin;
-Triangle(List<Integer> parameter,Point shapeOrigin)
-{
-	sideFirst=parameter.get(0);
-	sideSecond=parameter.get(1);
-	sideThird=parameter.get(2);
-	this.shapeOrigin=shapeOrigin;
-}
+	Triangle(List<Integer> parameter, Point shapeOrigin) {
+		sideFirst = parameter.get(0);
+		sideSecond = parameter.get(1);
+		sideThird = parameter.get(2);
+		this.shapeOrigin = shapeOrigin;
+	}
+	/*Using the shape method implemented by shape interface*/
+	public double getArea() {
+		double sideMean = (getPerimeter()) / 2;
+		return Math.sqrt(sideMean * (sideMean - sideFirst)
+				* (sideMean - sideSecond) * (sideMean - sideThird));
+	}
 
-public double getArea()
-{
-	double sideMean=(getPerimeter())/2;
-	return Math.sqrt(sideMean*(sideMean-sideFirst)*(sideMean-sideSecond)*(sideMean-sideThird));
-}
-public double getPerimeter()
-{
-return (sideFirst+sideSecond+sideThird);	
-}
-public Point getOrigin()
-{
-	return shapeOrigin;
-}
-public Shape.ShapeType type()
-{
-	return Shape.ShapeType.TRIANGLE;
-}
+	public double getPerimeter() {
+		return (sideFirst + sideSecond + sideThird);
+	}
 
-private double calculateArea(double xFirst,double yFirst,double xSecond,double ySecond,double xThird,double yThird)
-{
-	 return Math.abs((xFirst*(ySecond-yThird))+(xSecond*(yThird-yFirst))+(xThird*(yFirst-ySecond)))/2;	
-	
-	
-}
+	public Point getOrigin() {
+		return shapeOrigin;
+	}
 
+	public Shape.ShapeType type() {
+		return Shape.ShapeType.TRIANGLE;
+	}
 
+	private double calculateArea(double xFirst, double yFirst, double xSecond,
+			double ySecond, double xThird, double yThird) {
+		return Math.abs((xFirst * (ySecond - yThird))
+						+ (xSecond * (yThird - yFirst))
+						+ (xThird * (yFirst - ySecond))) / 2;
 
-public boolean isPointEnclosed(Point toCheck)
-{
-double xCheck=toCheck.getX();
-double yCheck=toCheck.getY();
+	}
 
-double leftX=shapeOrigin.getX();
-double leftY=shapeOrigin.getY();
+	public boolean isPointEnclosed(Point toCheck) {
+		/*These are the points to be checked for enclosing*/
+		double xCheck = toCheck.getX();
+		double yCheck = toCheck.getY();
 
-double rightX =sideThird+leftX;
-double rightY =leftY;
+		double leftX = shapeOrigin.getX();
+		double leftY = shapeOrigin.getY();
 
-double topX=(leftX+rightX)/2;
+		double rightX = sideThird + leftX;
+		double rightY = leftY;
+        //Calculating the angle and side using cosine formula
+		double topX = (leftX + rightX) / 2;
+		double angle = Math.acos(((sideFirst * sideFirst)
+				+ (sideThird * sideThird) - (sideSecond * sideSecond))
+				/ (2 * sideFirst * sideThird));
+		double height = Math.sin(angle) * sideFirst;
+		double topY = leftY + height;
 
-double angle=Math.acos((sideFirst*sideFirst)+(sideThird*sideThird)-(sideSecond*sideSecond)/(2*sideFirst*sideThird));
-double  height=Math.sin(angle)*sideFirst;
+		double areaFirst = calculateArea(xCheck, yCheck, rightX, rightY, topX,
+				topY);
+		double areaSecond = calculateArea(leftX, leftY, xCheck, yCheck, rightX,
+				rightY);
+		double areaThird = calculateArea(leftX, leftY, rightX, rightY, xCheck,
+				yCheck);
 
-double topY=leftY+height;
+		double sum = areaFirst + areaSecond + areaThird;
+		if (sum == getArea())
+			return true;
+		else
+			return false;
 
-
-double areaFirst=calculateArea(xCheck,yCheck,rightX,rightY,topX,topY);
-double areaSecond=calculateArea(leftX,leftY,xCheck,yCheck,rightX,rightY);
-double areaThird=calculateArea(leftX,leftY,rightX,rightY,xCheck,yCheck);
-
-double sum=areaFirst+areaSecond+areaThird;
-if(sum==getArea())
-	return true;
-else
-	return false;
-	
-	
-}
-	
-	
-	
-	
+	}
 
 }
