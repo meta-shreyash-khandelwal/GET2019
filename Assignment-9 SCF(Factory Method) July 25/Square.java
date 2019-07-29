de10cda84daeb668ae6a752e78c2fcd0205e4882
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-29 11:51:18 +0530

    Update Square.java

--- Assignment-9 SCF(Factory Method) July 25/Square.java
+++ Assignment-9 SCF(Factory Method) July 25/Square.java
@@ -1,48 +1,42 @@
-package varunk;
 import java.util.*;
+
 public class Square implements Shape {
-private double side;
-Point   shapeOrigin;
-Square(List<Integer> parameter,Point shapeOrigin)
-{
-	this.side=parameter.get(0);
-	this.shapeOrigin=shapeOrigin;
-}
+	private double side;
+	private Point shapeOrigin;
 
-public double getArea()
-{
-	return side*side;
-}
-public double getPerimeter()
-{
-return 4*side;	
-}
-public Point getOrigin()
-{
-	return shapeOrigin;
-}
-public Shape.ShapeType type()
-{
-	return Shape.ShapeType.SQUARE;
-}
-public boolean isPointEnclosed(Point toCheck)
-{
-	double xOrigin=shapeOrigin.getX();
-	double yOrigin=shapeOrigin.getY();
-	double xCheck=toCheck.getX();
-	double yCheck=toCheck.getY();
-	if(xCheck>= xOrigin && xCheck<=(xOrigin+side))
-	{
-		if(yCheck>= yOrigin && yCheck<=(yOrigin+side))
-		{
-			return true;
-		}
-			
+	Square(List<Integer> parameter, Point shapeOrigin) {
+		this.side = parameter.get(0);
+		this.shapeOrigin = shapeOrigin;
 	}
-	return false;
-}	
-	
-	
-}
-	
+	/*Implementing all the methods implemented in Shape interface*/
+	public double getArea() {
+		return side * side;
+	}
 
+	public double getPerimeter() {
+		return 4 * side;
+	}
+
+	public Point getOrigin() {
+		return shapeOrigin;
+	}
+
+	public Shape.ShapeType type() {
+		return Shape.ShapeType.SQUARE;
+	}
+
+	public boolean isPointEnclosed(Point toCheck) {
+		double xOrigin = shapeOrigin.getX();
+		double yOrigin = shapeOrigin.getY();
+		double xCheck = toCheck.getX();
+		double yCheck = toCheck.getY();
+		if (xCheck >= xOrigin && xCheck <= (xOrigin + side)) {
+			if (yCheck >= yOrigin && yCheck <= (yOrigin + side)) {
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+}
