1d61c6da52d78690720f679e6f86f9f0924a4338
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-29 11:48:43 +0530

    Update Factory.java

--- Assignment-9 SCF(Factory Method) July 25/Factory.java
+++ Assignment-9 SCF(Factory Method) July 25/Factory.java
@@ -1,22 +1,21 @@
-package varunk;
 import java.util.*;
+
 public class Factory {
 	public static Shape createShape(Shape.ShapeType type,
 			List<Integer> parameter, Point object) {
-		switch (type) {
-			case SQUARE :
-				return new Square(parameter, object);
-
-			case RECTANGLE :
-				return new Rectangle(parameter, object);
-			case POLYGON :
-				return new Polygon(parameter, object);
-			case CIRCLE :
-				return new Circle(parameter, object);
-			case TRIANGLE :
-				return new Triangle(parameter, object);
-			default :
-				return null;
+		switch (type) {//Using the factory design pattern to return the object of the specified type only on basis of user
+		case SQUARE:
+			return new Square(parameter, object);
+		case RECTANGLE:
+			return new Rectangle(parameter, object);
+		case POLYGON:
+			return new Polygon(parameter, object);
+		case CIRCLE:
+			return new Circle(parameter, object);
+		case TRIANGLE:
+			return new Triangle(parameter, object);
+		default:
+			return null;
 		}
 
 	}
