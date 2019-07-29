4c5e8b6c0bde91f5a35461c1a705674fc8f2a2c3
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-29 11:49:48 +0530

    Update Rectangle.java

--- Assignment-9 SCF(Factory Method) July 25/Rectangle.java
+++ Assignment-9 SCF(Factory Method) July 25/Rectangle.java
@@ -1,50 +1,43 @@
-package varunk;
 import java.util.*;
 public class Rectangle implements Shape {
-private double length;
-private double breadth;
-Point   shapeOrigin;
-Rectangle(List<Integer> parameter,Point shapeOrigin)
-{
-	length=parameter.get(0);
-	breadth=parameter.get(1);
-	this.shapeOrigin=shapeOrigin;
-}
+	private double length;
+	private double breadth;
+	private Point shapeOrigin;
 
-public double getArea()
-{
-	return length*breadth;
-}
-public double getPerimeter()
-{
-return 2*(length+breadth);	
-}
-public Point getOrigin()
-{
-	return shapeOrigin;
-}
-public Shape.ShapeType type()
-{
-	return Shape.ShapeType.RECTANGLE;
-}
-public boolean isPointEnclosed(Point toCheck)
-{
-	double xOrigin=shapeOrigin.getX();
-	double yOrigin=shapeOrigin.getY();
-	double xCheck=toCheck.getX();
-	double yCheck=toCheck.getY();
-	if(xCheck>= xOrigin && xCheck<=(xOrigin+length))
-	{
-		if(yCheck>= yOrigin && yCheck<=(yOrigin+breadth))
-		{
-			return true;
-		}
-			
+	Rectangle(List<Integer> parameter, Point shapeOrigin) {
+		length = parameter.get(0);
+		breadth = parameter.get(1);
+		this.shapeOrigin = shapeOrigin;
 	}
-	return false;
-}	
-	
-	
-	
+/*Implementing all the methods implemented in Shape interface*/
+	public double getArea() {
+		return length * breadth;
+	}
+
+	public double getPerimeter() {
+		return 2 * (length + breadth);
+	}
+
+	public Point getOrigin() {
+		return shapeOrigin;
+	}
+
+	public Shape.ShapeType type() {
+		return Shape.ShapeType.RECTANGLE;
+	}
+
+	public boolean isPointEnclosed(Point toCheck) {
+		double xOrigin = shapeOrigin.getX();
+		double yOrigin = shapeOrigin.getY();
+		double xCheck = toCheck.getX();
+		double yCheck = toCheck.getY();
+		if (xCheck >= xOrigin && xCheck <= (xOrigin + length)) {
+			if (yCheck >= yOrigin && yCheck <= (yOrigin + breadth)) {
+				return true;
+			}
+
+		}
+		return false;
+	}
 
 }
