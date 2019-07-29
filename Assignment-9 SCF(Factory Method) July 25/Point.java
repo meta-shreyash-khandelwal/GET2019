165552898c43801be221953b7a76dc753a060a9e
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-07-29 11:49:08 +0530

    Update Point.java

--- Assignment-9 SCF(Factory Method) July 25/Point.java
+++ Assignment-9 SCF(Factory Method) July 25/Point.java
@@ -1,18 +1,18 @@
-package varunk;
-
 public class Point {
-private double x;
-private double y;
-public Point(double x,double y)
-{
-this.x=x;
-this.y=y;
-}
-public double getX() {
-	return x;
-}
-public double getY() {
-	return y;
-}
+	private double x;
+	private double y;
+/*Getters method*/
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
 
 }
