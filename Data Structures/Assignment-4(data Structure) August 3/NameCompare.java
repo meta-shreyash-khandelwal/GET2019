d0e4f8f15120db7edac84ee5534b702466127a09
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-03 08:44:03 +0530

    Delete NameCompare.java

--- Data Structures/Assignment-4(data Structure) August 3/NameCompare.java
+++
@@ -1,8 +0,0 @@
-package varunk;
-import java.util.*;
-public class NameCompare implements Comparator<Employee> {
-	public int compare(Employee object1, Employee object2) 
-    { 
-        return object1.getName().compareTo(object2.getName()); 
-    } 
-}
