685f6969b83567dcd9f5135cbb83e50638ff3b52
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-03 17:46:13 +0530

    Update NameCompare.java

--- Data Structures/Assignment-4(data Structure) August 3/Part-1/NameCompare.java
+++ Data Structures/Assignment-4(data Structure) August 3/Part-1/NameCompare.java
@@ -1,6 +1,7 @@
 package varunk;
 import java.util.*;
 public class NameCompare implements Comparator<Employee> {
+	//Over-riding compare method
 	public int compare(Employee object1, Employee object2) 
     { 
         return object1.getName().compareTo(object2.getName()); 
