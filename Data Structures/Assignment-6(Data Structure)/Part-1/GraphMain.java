0d4c4b3f46d3242e6b0da5ce2c8b6a9729ccd660
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-09 17:00:41 +0530

    Update GraphMain.java

--- Data Structures/Assignment-6(Data Structure)/Part-1/GraphMain.java
+++ Data Structures/Assignment-6(Data Structure)/Part-1/GraphMain.java
@@ -14,13 +14,13 @@ public class GraphMain {
 		graph.addEdgeToGraph(2, 3, 4);
 		graph.addEdgeToGraph(3, 4, 2);
 		graph.addEdgeToGraph(4, 2, 4);
-		// graph.addEdgeToGraph(4, 1, 4);
+		graph.addEdgeToGraph(4, 1, 4);
 		// graph.addEdgeToGraph(4, 5, 6);
 		graph.printGraph();
 		graph.dfsStack(0);
 		System.out.println(graph.isConnected(0));
 		List<Integer> list = new ArrayList<Integer>();
-		list = graph.reachable(1);
+		list = graph.reachable(4);
 		for (int i = 0; i < list.size(); i++)
 			System.out.println("Reached-->" + list.get(i));
 		System.out.println("Minimum Spanning Tree: ");
