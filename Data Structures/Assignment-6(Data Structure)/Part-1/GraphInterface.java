cb60043aa73247361d63f1f991519929686c1d30
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-09 16:59:03 +0530

    Update GraphInterface.java

--- Data Structures/Assignment-6(Data Structure)/Part-1/GraphInterface.java
+++ Data Structures/Assignment-6(Data Structure)/Part-1/GraphInterface.java
@@ -7,7 +7,7 @@ public interface GraphInterface {
 	 public boolean isConnected(int source);
 	  public List<Integer> reachable(int vertex);
 	  public List<Edge> kruskalMST();
-	  public void dijkstraGetMinDistances(int sourceVertex);
+	  public List<Integer> dijkstraGetMinDistances(int sourceVertex);
 }
 
 
