cdf1fce92674aa1454a9f518783bc90e3a8e628c
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-22 16:02:19 +0530

    Update Jdbc4.java

--- DBMS/Assignment-5(DBMS)August20/Jdbc4.java
+++ DBMS/Assignment-5(DBMS)August20/Jdbc4.java
@@ -1,17 +1,12 @@
 package dbms;
-
-import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Jdbc4 {
     public static Connection connect;
   public static void main(String[] argv) {
       try
         {
-         connectionQuery();
+    	  connect=JDBCConnection.connectionQuery("storefront", "root", "root");
    
          String sqlQuery1="SET FOREIGN_KEY_CHECKS=0";;
    
@@ -21,11 +16,11 @@ public class Jdbc4 {
          
          String sqlQuery="delete from products where products.productid"
             		+ " in(select * from (select products.productid from products left join cart on "
-            		+ "cart.productid=products.productid where cart.orderid is null)as t);";//To prevent post delete update select * is used in front
+            		+ "cart.productid=products.productid where cart.orderid is null)as t);";
       
             PreparedStatement statement = connect.prepareStatement(sqlQuery);
               
-            int numRowsAffected = statement.executeUpdate(sqlQuery);//Returns the total number of rows deleted
+            int numRowsAffected = statement.executeUpdate(sqlQuery);
             connect.close();//Closing the connection
         System.out.println("The total number of records deleted are "+numRowsAffected);
          }
@@ -37,33 +32,4 @@ public class Jdbc4 {
         }
   }
 
-  public static void connectionQuery()
-  {
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/storefront","root","root");
-            System.out.println("Remote DB connection established");
-        }
-        catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (NullPointerException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote db connection establishment error");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            System.out.println("False query");
-        }
-    }
 }
