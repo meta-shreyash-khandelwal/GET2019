931d9b3dee7b578daef00e1fd26b6645cc2ff456
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-22 16:01:24 +0530

    Update Jdbc1.java

--- DBMS/Assignment-5(DBMS)August20/Jdbc1.java
+++ DBMS/Assignment-5(DBMS)August20/Jdbc1.java
@@ -1,19 +1,15 @@
 package dbms;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Jdbc1 {
     public static Connection connect;
   public static void main(String[] argv) {
       try
         {
-            connectionQuery();
-
-            PreparedStatement statement =  connect.prepareStatement("select orders.orderid,orders.date,sum(cart.Amount*cart.Quantity) as Amount "
+           connect=JDBCConnection.connectionQuery("storefront", "root", "root");
+                   PreparedStatement statement =  connect.prepareStatement("select orders.orderid,orders.date,sum(cart.Amount*cart.Quantity) as Amount "
             		+ "from Orders	inner join cart on orders.OrderID=cart.OrderID where cart.shipped=true "
             		+ "and orders.shoppperidr='s004' "
             		+ "group by orders.orderid"
@@ -44,33 +40,4 @@ public class Jdbc1 {
         }
   }
 
-  public static void connectionQuery()
-  {
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/storefront","root","root");
-            System.out.println("Remote DB connection established");
-        }
-        catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (NullPointerException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote db connection establishment error");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            System.out.println("False query");
-        }
-    }
 }
