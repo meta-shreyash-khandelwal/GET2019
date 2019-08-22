0ad1990f3b213697141ccf1358c8ac7541a8a4c1
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-08-22 16:01:45 +0530

    Update Jdbc2.java

--- DBMS/Assignment-5(DBMS)August20/Jdbc2.java
+++ DBMS/Assignment-5(DBMS)August20/Jdbc2.java
@@ -1,17 +1,15 @@
 package dbms;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Jdbc2 {
     public static Connection connect;
   public static void main(String[] argv) {
       try
         {
-            connectionQuery();
+    	  connect=JDBCConnection.connectionQuery("storefront", "root", "root");
 
             String sqlQuery="select count(categoryname) as child,parentcategorytitle from category where parentcategorytitle in ( select categoryname from category where parentcategorytitle ='Topcategory') group by parentcategorytitle";
             
@@ -39,33 +37,5 @@ public class Jdbc2 {
         }
   }
 
-  public static void connectionQuery()
-  {
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/storefront","root","root");
-            System.out.println("Remote DB connection established");
-        }
-        catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (NullPointerException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote server could not be connected");
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            System.out.println("Remote db connection establishment error");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            System.out.println("False query");
-        }
-    }
+ 
 }
