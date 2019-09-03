45e05351737c5eef4836a929170aea902281e76b
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-09-03 09:16:31 +0530

    Update UserController.java

--- EAD/EAD Assignment-5.1(September 2)/controller/UserController.java
+++ EAD/EAD Assignment-5.1(September 2)/controller/UserController.java
@@ -1,5 +1,27 @@
 package controller;
-
+import model.*;
+import facade.*;
 public class UserController {
+	UserFacade userFacade;
+	static private UserController userController;
+private UserController(){
+	userFacade=UserFacade.getInstance();
+	}
 
-}
+	public static UserController getInstance(){
+		if(userController==null){
+			return new UserController();
+		}
+		return userController;
+	}
+	
+	
+	public void addUser(User user){
+			
+			userFacade.addUser(user);
+		}
+	public User getUserByID(int userID)
+	{
+		return userFacade.getUserByID(userID);
+	}
+	}
