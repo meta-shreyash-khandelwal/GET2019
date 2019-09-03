472aac6369317c024e492521cbe4932422e97eae
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-09-03 09:14:55 +0530

    Update ProductDAO.java

--- EAD/EAD Assignment-5.1(September 2)/dao/ProductDAO.java
+++ EAD/EAD Assignment-5.1(September 2)/dao/ProductDAO.java
@@ -6,7 +6,12 @@ private static ProductDAO productdao;
 private List<Product> productList;
 private ProductDAO()
 {
-	
+productList.add(new Product("p1","Mobile","Electronics",25000,15));
+productList.add(new Product("p2","Shirt","Clothing",1250,50));
+productList.add(new Product("p3","T-shirt","Clothing",250,50));
+productList.add(new Product("p4","Calculator","Electronics",2500,10));
+productList.add(new Product("p5","Sport Shoes","Shoes",5000,60));
+productList.add(new Product("p6","Samsung TV","TV",50000,10));
 }
 public static ProductDAO getInstance()
 {
@@ -14,20 +19,10 @@ public static ProductDAO getInstance()
 		productdao=new ProductDAO();
 	return productdao;
 }
-public void addProduct(Product newProduct)
-{
-	productList.add(newProduct);
-}
-public void deleteProduct(Product existingProduct)
-{
-	productList.remove(existingProduct);
-}
-public List<Product> getAllProducts()
+public List<Product> getList()
 {
 	return productList;
 }
-
-
 }
 
 
