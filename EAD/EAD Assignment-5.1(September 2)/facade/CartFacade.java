13a5d4de522bdc3140f034a5b57fe975cbee908f
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-09-03 09:13:50 +0530

    Update CartFacade.java

--- EAD/EAD Assignment-5.1(September 2)/facade/CartFacade.java
+++ EAD/EAD Assignment-5.1(September 2)/facade/CartFacade.java
@@ -1,5 +1,72 @@
 package facade;
-
+import model.*;
+import java.util.*;
+import dao.CartDAO;
+import dao.ProductDAO;
 public class CartFacade {
+	static private CartFacade cartFacade;
+	static private ProductDAO productDAO;
+	private CartDAO cartDAO;
+	private CartFacade()
+	{
+		cartDAO=CartDAO.getInstance();
+		productDAO=ProductDAO.getInstance();
+	}
+	public static CartFacade getInstance()
+	{
+		if(cartFacade==null)
+			return new CartFacade();
+		return cartFacade;
+	}
+	
+public void addProductToCart(int userID,String productID,int quantity)
+{
+	Cart cart=cartDAO.getCart(userID);
+	List<Product> productList=productDAO.getList();
+	for(int i=0;i<productList.size();i++)
+	{
+		if(productList.get(i).getProductCode().equals(productID))
+		{
+			productList.get(i).setQuantity(quantity);
+			cart.addProduct(productList.get(i));
+			}
+	}
+	
+	
+}
+public void deleteProductFromCart(int userID,String productID)
+{
+	List<Product> productList=productDAO.getList();
+	Cart cart=cartDAO.getCart(userID);
+	for(int i=0;i<productList.size();i++)
+	{
+		if(productList.get(i).getProductCode().equals(productID))
+		{
+			cart.removeProduct(productList.get(i));
+			}
+	}
+}
+public void updateCart(int userID,String productID,int quantity)
+{
+	Cart cart=cartDAO.getCart(userID);
+	List<Product> productList=productDAO.getList();
+	for(int i=0;i<productList.size();i++)
+	{
+		if(productList.get(i).getProductCode().equals(productID))
+		{
+			cart.removeProduct(productList.get(i));
+			productList.get(i).setQuantity(quantity);
+			cart.addProduct(productList.get(i));
+			}
+	}
+	
+}
 
+public List<Product> getCartProduct(int userID)
+{
+	Cart cart=cartDAO.getCart(userID);
+	return cart.cartItems();
+}
+	
+	
 }
