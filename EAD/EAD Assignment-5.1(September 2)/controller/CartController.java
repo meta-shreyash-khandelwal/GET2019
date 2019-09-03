aac1535c3351605658c1fc62588faf13c848e2ed
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-09-03 09:17:17 +0530

    Update CartController.java

--- EAD/EAD Assignment-5.1(September 2)/controller/CartController.java
+++ EAD/EAD Assignment-5.1(September 2)/controller/CartController.java
@@ -1,5 +1,42 @@
 package controller;
-
+import model.*;
+import java.util.*;
+import dao.*;
+import facade.*;
 public class CartController {
+private static CartController cartController;
+private CartFacade cartFacade;
+private ProductController productController;
+public CartController() {
+		cartFacade=CartFacade.getInstance();
+		productController=ProductController.getInstance();
+	}
 
+	public static CartController getInstance(){
+		if(cartController==null){
+			return new CartController();
+		}
+		return cartController;
+	}
+
+	public List<Product> getCartList(int userID)
+	{
+		return cartFacade.getCartProduct(userID);
+	}
+
+	public void addProductToCart(int userID, String productID, int productQuantity) 
+	{
+		//Product product=productController.getProductById(productID);
+			
+		cartFacade.addProductToCart(userID, productID, productQuantity);
+	}
+
+	public void deleteProductFromCart(int userID,String productID) 
+	{
+cartFacade.deleteProductFromCart(userID, productID);
+	
+	
+}
+	
+	
 }
