dd4f023e8549eb62d706a76278b75cf05667974d
meta-shreyash-khandelwal <52688808+meta-shreyash-khandelwal@users.noreply.github.com>
2019-09-03 09:13:32 +0530

    Update ProductFacade.java

--- EAD/EAD Assignment-5.1(September 2)/facade/ProductFacade.java
+++ EAD/EAD Assignment-5.1(September 2)/facade/ProductFacade.java
@@ -1,5 +1,41 @@
 package facade;
-
+import dao.ProductDAO;
+import model.Product;
+import java.util.*;
 public class ProductFacade {
+static private ProductFacade productFacade;
+private ProductDAO productDAO;
+private ProductFacade()
+{
+	productDAO=ProductDAO.getInstance();
+}
+public static ProductFacade getInstance()
+{
+	if(productFacade==null)
+		return new ProductFacade();
+	return productFacade;
+}
 
+public List<Product> getProducts()
+{
+	return productDAO.getList();
+}
+
+public Product getproductByID(String productID)
+{
+	int i=0;
+	List<Product> listProduct=new ArrayList<Product>();
+	listProduct=getProducts();
+	for(i=0;i<listProduct.size();i++)
+	{
+		if(listProduct.get(i).getProductCode().equals(productID))
+			break;
+
+	}
+	return listProduct.get(i);
+}
+
+
+	
+	
 }
